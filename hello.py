df5c583a423b6baa30354b8c615b07f9cb2a7cda
Abhisek <abhisek.das@LEGO.com>
2023-08-15 09:57:57 +0200

    Updates

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 msg = "Roll a dice"
 print(msg)
-# Example 1: Looping through a list 1 and 2
+# Example 1: Looping through a list 1 and 2 and 4
 fruits = ['apple', 'banana', 'orange', 'grape']
 
 for fruit in fruits:
